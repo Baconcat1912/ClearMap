fdd9b3ea121d46b119f8c9249473233f0f5c0d51
Bacon <baconcat1912@gmail.com>
2025-06-19 11:56:48 +0800

    Start work on app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
+import re
+import networkx as nx
+from pyvis.network import Network
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello, world!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
